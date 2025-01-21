66c45ead2070dab6635d6577908c08f05c07eb00
刘鹏 <2254842466@qq.com>
2025-01-21 19:31:30 +0800

    utils 1.1.0

---
+++ packages/utils/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @lp-cli/utils
+
+## 0.1.0
+
+### Minor Changes
+
+- utisl
---
+++ packages/utils/src/test.ts
@@ -0,0 +1,19 @@
+import NpmPackage from './NpmPackage.js';
+import path from 'node:path';
+
+async function main() {
+  const pkg = new NpmPackage({
+    targetPath: path.join(import.meta.dirname, '../aaa'),
+    name: 'create-vite'
+  });
+
+  if (await pkg.exists()) {
+    pkg.update();
+  } else {
+    pkg.install();
+  }
+
+  console.log(await pkg.getPackageJSON());
+}
+
+main();
